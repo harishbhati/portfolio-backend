f29c5e7c5013f864cbece7e45cdb4eedbae63a0d
harish-bhati-ui <harishbhati.bhati@gmail.com>
2025-12-16 12:47:35 +0530

    db and server file change for deployement

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const dbConnection = () =>{
-    mongoose.connect(process.env.MONGO_URI, {
-        dbName: "PORTFOLIO", 
-    }).then(() => {
-        console.log("Database connected!");
-    }).catch((error)=>{
-        console.log(`Error while conection with datbase ${error}`)
-    })
-}
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "PORTFOLIO",
+    });
+    console.log("Database connected!");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
+  }
+};
 
 export default dbConnection;
