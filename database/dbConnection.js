0c5258d2144df0a2b739227ed9d8cea1547dc8e1
harish-bhati-ui <harishbhati.bhati@gmail.com>
2025-12-16 13:10:19 +0530

    render env file issue fix

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
